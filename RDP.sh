099740a878c05d3a4f6d2956dd385c170f93febe
ojasgujju <77567127+ojasgujju@users.noreply.github.com>
2021-04-12 11:35:50 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -18,7 +18,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser FSID chrome-remote-desktop
+sudo adduser AK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
